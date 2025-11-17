31e3e6d8144caf1a3573e7d8468bf69b8da4bfeb
Kirilok <kiriloki2018@gmail.com>
2025-11-17 23:49:05 +0200

    add single book page

--- app/shelf/[bookId]/page.js
+++ app/shelf/[bookId]/page.js
@@ -1,9 +1,12 @@
-export default function BookItem() {
-    return (
-        <>
-            <div>
-                <h1>Book item</h1>
-            </div>
-        </>
-    );
-}
+
+import BookItem from "@/components/bookItem";
+import { getBook } from "@/lib/book";
+
+export default async function Book({params}) {
+    const { bookId } = await params;
+
+    const book = await getBook(bookId);
+
+
+    return <BookItem book={book}/>
+};
